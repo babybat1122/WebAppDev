f275113bcdf4d5e411b6b118c83347b158a28fac
vicah <vicaheroczi@gmail.com>
2021-11-23 08:35:19 +0100

    SpringBoot alap

---
+++ IA_9_SpringBoot/IA_9/src/main/java/edu/corvinus/IA_9/Ia9Application.java
@@ -0,0 +1,13 @@
+package edu.corvinus.IA_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ia9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ia9Application.class, args);
+	}
+
+}
---
+++ IA_9_SpringBoot/IA_9/src/test/java/edu/corvinus/IA_9/Ia9ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.corvinus.IA_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ia9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
